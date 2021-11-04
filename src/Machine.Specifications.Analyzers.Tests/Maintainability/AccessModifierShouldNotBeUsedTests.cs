2b74f74e3f833f0e492fa9a8f95077c8eab9cb0e
Robert Coltheart <robert.coltheart@gmail.com>
2021-11-04 22:35:06 +0800

    tests

--- src/Machine.Specifications.Analyzers.Tests/Maintainability/AccessModifierShouldNotBeUsedTests.cs
+++ src/Machine.Specifications.Analyzers.Tests/Maintainability/AccessModifierShouldNotBeUsedTests.cs
@@ -9,33 +9,60 @@ namespace Machine.Specifications.Analyzers.Tests.Maintainability
     public class AccessModifierShouldNotBeUsedTests
     {
         [Fact]
+        public async Task NoErrorsInValidSource()
+        {
+            const string source = @"
+using System;
+using Machine.Specifications;
+
+namespace ConsoleApplication1
+{
+    class SpecsClass
+    {
+        static string value;
+
+        It should_do_something = () =>
+            true.ShouldBeTrue();
+
+        class inner_specs
+        {
+            Establish context = () =>
+                value = string.Empty;
+        }
+    }
+}";
+
+            await Verify.VerifyAnalyzerAsync(source);
+        }
+
+        [Fact]
         public async Task RemovesClassAccessModifier()
         {
             const string source = @"
-    using System;
-    using Machine.Specifications;
+using System;
+using Machine.Specifications;
 
-    namespace ConsoleApplication1
+namespace ConsoleApplication1
+{
+    public class {|#0:SpecsClass|}
     {
-        public class {|#0:SpecsClass|}
-        {
-            It should_do_something = () =>
-                true.ShouldBeTrue();
-        }
-    }";
+        It should_do_something = () =>
+            true.ShouldBeTrue();
+    }
+}";
 
             const string fixedSource = @"
-    using System;
-    using Machine.Specifications;
+using System;
+using Machine.Specifications;
 
-    namespace ConsoleApplication1
+namespace ConsoleApplication1
+{
+    class SpecsClass
     {
-        class SpecsClass
-        {
-            It should_do_something = () =>
-                true.ShouldBeTrue();
-        }
-    }";
+        It should_do_something = () =>
+            true.ShouldBeTrue();
+    }
+}";
 
             var expected = Verify.Diagnostic(DiagnosticIds.Maintainability.AccessModifierShouldNotBeUsed)
                 .WithLocation(0)
@@ -48,30 +75,30 @@ namespace Machine.Specifications.Analyzers.Tests.Maintainability
         public async Task RemovesFieldAccessModifier()
         {
             const string source = @"
-    using System;
-    using Machine.Specifications;
+using System;
+using Machine.Specifications;
 
-    namespace ConsoleApplication1
+namespace ConsoleApplication1
+{
+    class SpecsClass
     {
-        class SpecsClass
-        {
-            private It {|#0:should_do_something|} = () =>
-                true.ShouldBeTrue();
-        }
-    }";
+        private It {|#0:should_do_something|} = () =>
+            true.ShouldBeTrue();
+    }
+}";
 
             const string fixedSource = @"
-    using System;
-    using Machine.Specifications;
+using System;
+using Machine.Specifications;
 
-    namespace ConsoleApplication1
+namespace ConsoleApplication1
+{
+    class SpecsClass
     {
-        class SpecsClass
-        {
-            It should_do_something = () =>
-                true.ShouldBeTrue();
-        }
-    }";
+        It should_do_something = () =>
+            true.ShouldBeTrue();
+    }
+}";
 
             var expected = Verify.Diagnostic(DiagnosticIds.Maintainability.AccessModifierShouldNotBeUsed)
                 .WithLocation(0)
@@ -84,40 +111,103 @@ namespace Machine.Specifications.Analyzers.Tests.Maintainability
         public async Task RemovesFieldAndClassAccessModifiers()
         {
             const string source = @"
-    using System;
-    using Machine.Specifications;
+using System;
+using Machine.Specifications;
 
-    namespace ConsoleApplication1
+namespace ConsoleApplication1
+{
+    public class {|#0:SpecsClass|}
     {
-        public class {|#0:SpecsClass|}
-        {
-            private It {|#1:should_do_something|} = () =>
-                true.ShouldBeTrue();
-        }
-    }";
+        private It {|#1:should_do_something|} = () =>
+            true.ShouldBeTrue();
+    }
+}";
 
             const string fixedSource = @"
-    using System;
-    using Machine.Specifications;
+using System;
+using Machine.Specifications;
 
-    namespace ConsoleApplication1
+namespace ConsoleApplication1
+{
+    class SpecsClass
     {
-        class SpecsClass
-        {
-            It should_do_something = () =>
-                true.ShouldBeTrue();
+        It should_do_something = () =>
+            true.ShouldBeTrue();
+    }
+}";
+
+            var expected = new[]
+            {
+                Verify.Diagnostic(DiagnosticIds.Maintainability.AccessModifierShouldNotBeUsed)
+                    .WithLocation(0)
+                    .WithArguments("SpecsClass"),
+
+                Verify.Diagnostic(DiagnosticIds.Maintainability.AccessModifierShouldNotBeUsed)
+                    .WithLocation(1)
+                    .WithArguments("should_do_something")
+            };
+
+            await Verify.VerifyCodeFixAsync(source, expected, fixedSource);
         }
-    }";
 
-            var expectedClass = Verify.Diagnostic(DiagnosticIds.Maintainability.AccessModifierShouldNotBeUsed)
-                .WithLocation(0)
-                .WithArguments("SpecsClass");
+        [Fact]
+        public async Task RemovesInnerFieldAndClassAccessModifiers()
+        {
+            const string source = @"
+using System;
+using Machine.Specifications;
 
-            var expectedField = Verify.Diagnostic(DiagnosticIds.Maintainability.AccessModifierShouldNotBeUsed)
-                .WithLocation(1)
-                .WithArguments("should_do_something");
+namespace ConsoleApplication1
+{
+    class SpecsClass
+    {
+        private static string {|#0:value|};
 
-            await Verify.VerifyCodeFixAsync(source, new[] {expectedClass, expectedField}, fixedSource);
+        It should_do_something = () =>
+            true.ShouldBeTrue();
+
+        internal class {|#1:InnerClass|}
+        {
+            private Establish {|#2:context|} = () =>
+                value = string.Empty;
+        }
+    }
+}";
+
+            const string fixedSource = @"
+using System;
+using Machine.Specifications;
+
+namespace ConsoleApplication1
+{
+    class SpecsClass
+    {
+        static string value;
+
+        It should_do_something = () =>
+            true.ShouldBeTrue();
+
+        class InnerClass
+        {
+            Establish context = () =>
+                value = string.Empty;
+        }
+    }
+}";
+
+            var expected = new[]
+            {
+                Verify.Diagnostic()
+                    .WithLocation(0),
+
+                Verify.Diagnostic()
+                    .WithLocation(1),
+
+                Verify.Diagnostic()
+                    .WithLocation(2)
+            };
+
+            await Verify.VerifyCodeFixAsync(source, expected, fixedSource);
         }
     }
 }
