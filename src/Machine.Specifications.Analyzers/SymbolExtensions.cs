609a1f5085e337da0c1ab89b98e518506834cc5d
Robert Coltheart <robert.coltheart@gmail.com>
2021-11-06 21:38:23 +0800

    unused

--- src/Machine.Specifications.Analyzers/SymbolExtensions.cs
+++ src/Machine.Specifications.Analyzers/SymbolExtensions.cs
@@ -4,26 +4,6 @@ namespace Machine.Specifications.Analyzers
 {
     public static class SymbolExtensions
     {
-        public static bool IsAssignableFrom(this ITypeSymbol symbol, ITypeSymbol source)
-        {
-            if (symbol is null)
-            {
-                return false;
-            }
-
-            while (source is not null)
-            {
-                if (source.Equals(symbol, SymbolEqualityComparer.Default))
-                {
-                    return true;
-                }
-
-                source = source.BaseType;
-            }
-
-            return false;
-        }
-
         public static bool IsMember(this ITypeSymbol symbol)
         {
             return symbol.IsMspecAssembly() &&
